fbd761f195a8d931759737c40e7b50bd9224e447
AonghasMackay <aonghasmackay@yahoo.co.uk>
2023-10-12 22:01:54 +0100

    init commit

---
+++ database/migrations/2023_10_12_203517_init_schema.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //as its an initial migration, we can just drop all tables
+        Schema::dropIfExists('leaderboard');
+        Schema::dropIfExists('user_stats');
+        Schema::dropIfExists('user_score');
+        Schema::dropIfExists('users');
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('user_score', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->unique();
+            $table->foreign('user_id')->references('user_id')->on('users');
+
+            $table->integer('score')->default(0);
+            $table->integer('balance')->default(5);
+        });
+
+        Schema::create('user_stats', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->unique();
+            $table->foreign('user_id')->references('user_id')->on('users');
+
+            $table->integer('number_of_wins')->default(0);
+            $table->integer('number_of_runner_ups')->default(0);
+        });
+
+        Schema::create('leaderboard', function (Blueprint $table) {
+            $table->id();
+
+            $table->date('end_date');
+
+            $table->string('winner_id');
+            $table->foreign('winner_id')->references('user_id')->on('users');
+
+            $table->string('runner_up_id');
+            $table->foreign('runner_up_id')->references('user_id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaderboard');
+        Schema::dropIfExists('user_stats');
+        Schema::dropIfExists('user_score');
+        Schema::dropIfExists('users');
+    }
+};
